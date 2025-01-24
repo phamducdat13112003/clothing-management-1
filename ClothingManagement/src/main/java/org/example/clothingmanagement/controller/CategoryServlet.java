24a68bf89ec6968f410c3d3efb1b832363e03298
cuongsher <cuongsher@gmail.com>
2025-01-24 21:06:36 +0700

    Add CategoryServlet

---
+++ ClothingManagement/src/main/java/org/example/clothingmanagement/controller/CategoryServlet.java
@@ -0,0 +1,33 @@
+package org.example.clothingmanagement.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.example.clothingmanagement.entity.*;
+import org.example.clothingmanagement.service.*;
+
+
+import java.io.IOException;
+
+import java.util.List;
+
+
+@WebServlet(name = "CategoryServlet", value = "/CategoryServlet")
+public class CategoryServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, ServletException {
+        List<Category> categories = null;
+        categories = CategoryDAO.selectAll();
+        request.setAttribute("categories", categories);
+        request.getRequestDispatcher("Category.jsp").forward(request, response);
+
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+
+}
