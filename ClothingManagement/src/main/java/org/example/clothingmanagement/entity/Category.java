9b68350e8b7b5e0a02101ddccdd2cca9f350106e
cuongsher <cuongsher@gmail.com>
2025-01-24 21:07:05 +0700

    Add Entity Category

---
+++ ClothingManagement/src/main/java/org/example/clothingmanagement/entity/Category.java
@@ -0,0 +1,17 @@
+package org.example.clothingmanagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    private int categoryID;
+    private String categoryName;
+    private Date createdDate;
+    private int createdBy;
+}
