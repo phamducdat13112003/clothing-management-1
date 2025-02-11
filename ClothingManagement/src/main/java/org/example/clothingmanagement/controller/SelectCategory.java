ab937b1bf431667ec1f2f747fef9110eaac123cd
cuongsher <cuongsher@gmail.com>
2025-02-11 23:46:57 +0700

    category search

--- ClothingManagement/src/main/java/org/example/clothingmanagement/controller/SelectCategory.java
+++ ClothingManagement/src/main/java/org/example/clothingmanagement/controller/SelectCategory.java
@@ -6,14 +6,15 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.example.clothingmanagement.entity.Category;
-import org.example.clothingmanagement.repository.CategoryDAO;
 import org.example.clothingmanagement.service.CategoryService;
 
 import java.io.IOException;
 
 import java.util.ArrayList;
 import java.sql.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @WebServlet(name = "SelectCategory", value = "/SelectCategory")
@@ -22,7 +23,7 @@ public class SelectCategory extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         CategoryService dao = new CategoryService();
         List<Category> categories = new ArrayList<>();
-
+        Map<Integer, String> createdByNames = null;
         try {
             // Lấy dữ liệu từ form
             String categoryName = request.getParameter("categoryName");
@@ -39,17 +40,22 @@ public class SelectCategory extends HttpServlet {
                 createdBy = dao.getEmployeeIDByName(createdByStr);
 
             }
-
             // Lọc dữ liệu
             categories = dao.filterCategories(categoryName, startDate, endDate, createdBy);
+            createdByNames = new HashMap<>();
+            for (Category category : categories) {
+                createdBy = category.getCreatedBy();
+                if (!createdByNames.containsKey(createdBy)) {
+                    createdByNames.put(createdBy, CategoryService.getEmployeeNameByCreatedBy(createdBy));
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         // Gửi danh sách danh mục sang JSP
         request.setAttribute("categories", categories);
+        request.setAttribute("createdByNames", createdByNames);
         request.getRequestDispatcher("Category.jsp").forward(request, response);
-
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
