ced08cef4ccac743ed39423a31701ebc10d9fbff
phamducdat13 <datpdhs176052@fpt.edu.vn>
2025-02-28 13:14:03 +0700

    update atribute product

--- ClothingManagement/src/main/java/org/example/clothingmanagement/entity/Product.java
+++ ClothingManagement/src/main/java/org/example/clothingmanagement/entity/Product.java
@@ -15,22 +15,25 @@ public class Product {
     private String id;
     private String name;
     private Double price;
-    private Integer binId;
-    private Integer categoryId;
     private String material;
     private String gender;
     private String seasons;
     private Integer minQuantity;
     private Date createdDate;
     private String description;
-    private Integer createdBy;
-    private Integer supplierId;
     private String madeIn;
+    private Integer binId;
+    private Integer categoryId;
+    private String createdBy;
+    private String supplierId;
+    private int Status;
 
+
+    //Thừa
     private Integer totalQuantity;
     private String urlImage;
 
-    public Product(String name, Double price, String seasons, Integer supplierId, String material, String madeIn, String gender, String description, Integer categoryId, Integer minQuantity,Integer BinId,Integer createdBy) {
+    public Product(String name, Double price, String seasons, String supplierId, String material, String madeIn, String gender, String description, Integer categoryId, Integer minQuantity,Integer BinId,String createdBy) {
         this.name = name;
         this.price = price;
         this.seasons = seasons;
@@ -43,5 +46,20 @@ public class Product {
         this.minQuantity = minQuantity;
         this.binId = BinId;
         this.createdBy = createdBy;
+    }
+    public Product(String name, Double price, String seasons, String supplierId, String material, String madeIn, String gender, String description, Integer categoryId, Integer minQuantity,Integer BinId,String createdBy, int Status) {
+        this.name = name;
+        this.price = price;
+        this.seasons = seasons;
+        this.supplierId = supplierId;
+        this.material = material;
+        this.madeIn = madeIn;
+        this.gender = gender;
+        this.description = description;
+        this.categoryId = categoryId;
+        this.minQuantity = minQuantity;
+        this.binId = BinId;
+        this.createdBy = createdBy;
+        this.Status = Status;
     }
 }
