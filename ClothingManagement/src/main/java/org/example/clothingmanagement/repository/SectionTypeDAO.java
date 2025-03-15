8e45658e37d7a19d2b4730e93ce6e3a2dc25047b
ducnthe181557 <ducnthe181557@fpt.edu.vn>
2025-03-16 01:05:06 +0700

    update pagination and search in list-section-type

--- ClothingManagement/src/main/java/org/example/clothingmanagement/repository/SectionTypeDAO.java
+++ ClothingManagement/src/main/java/org/example/clothingmanagement/repository/SectionTypeDAO.java
@@ -33,4 +33,107 @@ public class SectionTypeDAO {
             throw new RuntimeException(e);
         }
     }
+
+    public List<SectionType> getSectionTypesWithPagination(int page, int pageSize) {
+        try(Connection con = DBContext.getConnection()){
+            StringBuilder sql = new StringBuilder();
+            sql.append(" SELECT SectionTypeID, SectionTypeName, WarehouseID, Description ");
+            sql.append(" FROM SectionType ");
+            sql.append(" ORDER BY SectionTypeID");
+            sql.append(" LIMIT ? OFFSET ? ");
+            PreparedStatement ps = con.prepareStatement(sql.toString());
+            ps.setInt(1, pageSize);
+            ps.setInt(2, (page-1)*pageSize);
+            ResultSet rs = ps.executeQuery();
+            List<SectionType> sectionTypes = new ArrayList<>();
+            while (rs.next()) {
+                SectionType sectionType = SectionType.builder()
+                        .sectionTypeId(rs.getInt("SectionTypeID"))
+                        .sectionTypeName(rs.getString("SectionTypeName"))
+                        .warehouseId(rs.getString("WarehouseID"))
+                        .description(rs.getString("Description"))
+                        .build();
+                sectionTypes.add(sectionType);
+            }
+            return sectionTypes;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<SectionType> searchSectionTypes(String search) {
+        try(Connection con = DBContext.getConnection()){
+            StringBuilder sql = new StringBuilder();
+            sql.append(" SELECT SectionTypeID, SectionTypeName, WarehouseID, Description ");
+            sql.append(" FROM SectionType ");
+            sql.append(" WHERE 1=1 ");
+
+            if(!search.isEmpty()){
+                sql.append(" AND (SectionTypeName LIKE ? ");
+                sql.append(" OR Description LIKE ?) ");
+            }
+            sql.append(" ORDER BY SectionTypeID");
+
+            PreparedStatement ps = con.prepareStatement(sql.toString());
+            int paramIndex = 1;
+            if(!search.isEmpty()){
+                ps.setString(paramIndex++, "%"+search+"%");
+                ps.setString(paramIndex++, "%"+search+"%");
+            }
+            ResultSet rs = ps.executeQuery();
+            List<SectionType> sectionTypes = new ArrayList<>();
+            while (rs.next()) {
+                SectionType sectionType = SectionType.builder()
+                        .sectionTypeId(rs.getInt("SectionTypeID"))
+                        .sectionTypeName(rs.getString("SectionTypeName"))
+                        .warehouseId(rs.getString("WarehouseID"))
+                        .description(rs.getString("Description"))
+                        .build();
+                sectionTypes.add(sectionType);
+            }
+            return sectionTypes;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<SectionType> searchSectionTypesWithPagination(String search,int page, int pageSize) {
+        try(Connection con = DBContext.getConnection()){
+            StringBuilder sql = new StringBuilder();
+            sql.append(" SELECT SectionTypeID, SectionTypeName, WarehouseID, Description ");
+            sql.append(" FROM SectionType ");
+            sql.append(" WHERE 1=1 ");
+            if(!search.isEmpty()){
+                sql.append(" AND (SectionTypeName LIKE ? ");
+                sql.append(" OR Description LIKE ?) ");
+            }
+            sql.append(" ORDER BY SectionTypeID");
+            sql.append(" LIMIT ? OFFSET ? ");
+            PreparedStatement ps = con.prepareStatement(sql.toString());
+            int paramIndex = 1;
+            if(!search.isEmpty()){
+                ps.setString(paramIndex++, "%"+search+"%");
+                ps.setString(paramIndex++, "%"+search+"%");
+            }
+            ps.setInt(paramIndex++, pageSize);
+            ps.setInt(paramIndex++, (page-1)*pageSize);
+            ResultSet rs = ps.executeQuery();
+            List<SectionType> sectionTypes = new ArrayList<>();
+            while (rs.next()) {
+                SectionType sectionType = SectionType.builder()
+                        .sectionTypeId(rs.getInt("SectionTypeID"))
+                        .sectionTypeName(rs.getString("SectionTypeName"))
+                        .warehouseId(rs.getString("WarehouseID"))
+                        .description(rs.getString("Description"))
+                        .build();
+                sectionTypes.add(sectionType);
+            }
+            return sectionTypes;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- ClothingManagement/src/main/java/org/example/clothingmanagement/service/SectionTypeService.java
+++ ClothingManagement/src/main/java/org/example/clothingmanagement/service/SectionTypeService.java
@@ -10,4 +10,24 @@ public class SectionTypeService {
     public List<SectionType> getAllSectionTypes() {
         return std.getAllSectionTypes();
     }
+    public List<SectionType> getSectionTypesWithPagination(int page, int pageSize) {
+        return std.getSectionTypesWithPagination(page, pageSize);
+    }
+
+    public List<SectionType> searchSectionTypes(String search) {
+        return std.searchSectionTypes(search);
+    }
+
+    public List<SectionType> searchSectionTypesWithPagination(String search, int page, int pageSize) {
+        return std.searchSectionTypesWithPagination(search, page, pageSize);
+    }
+
+    public static void main(String[] args){
+        SectionTypeService sts = new SectionTypeService();
+        List<SectionType> list = sts.searchSectionTypesWithPagination("ss",1,5);
+        List<SectionType> list2 = sts.searchSectionTypes("ss");
+        for(SectionType st : list2){
+            System.out.println(st);
+        }
+    }
 }
