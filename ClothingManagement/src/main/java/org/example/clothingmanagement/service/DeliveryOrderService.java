48f724be156338ebf1f3ea374e8ccac95e38af8c
phamducdat13 <datpdhs176052@fpt.edu.vn>
2025-03-16 00:44:43 +0700

    DO tàn dư comit lên tránh ae lỗi

--- ClothingManagement/src/main/java/org/example/clothingmanagement/service/DeliveryOrderService.java
+++ ClothingManagement/src/main/java/org/example/clothingmanagement/service/DeliveryOrderService.java
@@ -1,7 +1,7 @@
 package org.example.clothingmanagement.service;
 
-public class DeliveryOrderService {
-    private static final DeliveryOrderDAO DoDAO = new DeliveryOrderDAO();
-
-
-}
+//public class DeliveryOrderService {
+//    private static final DeliveryOrderDAO DoDAO = new DeliveryOrderDAO();
+//
+//
+//}
